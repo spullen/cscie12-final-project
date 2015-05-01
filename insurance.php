e57ac64a459d1bb8000051afebbfaef1a21a23fd
Scott Pullen <s.pullen05@gmail.com>
2015-04-30 20:35:59 -0400

    Adding insurance page

--- insurance.php
+++ insurance.php
@@ -5,7 +5,30 @@
 <?php include 'includes/header.php' ?>
 <div class="grid-container">
   <div class="grid-100">
-    <h1>Hello</h1>
+    <h2>Insurance</h2>
+    <h3>In-Network Insurance Plans:</h3>
+    <ul>
+      <li>Altus</li>
+      <li>Delta Dental</li>
+      <li>Guardian</li>
+      <li>Harvard Pilgrim</li>
+      <li>Principal</li>
+      <li>Tufts Dental</li>
+    </ul>
+    <h3>Out-Of-Network Insurance Plans:</h3>
+    <ul>
+      <li>Aetna</li>
+      <li>Metlife</li>
+      <li>Tricare</li>
+      <li>United Health Care</li>
+    </ul>
+    <p>
+      If you have an insurance plan other than the plans listed above, you will be responsible for paying for services. We can 
+      provide you with the appropriate forms that you can submit to your insurance provider.
+    </p>
+    <p>
+      If you do not have an insurance plan, you will be responsible for paying for services due at each visit.
+    </p>
   </div>
 </div>
 <?php include 'includes/footer.php' ?>
