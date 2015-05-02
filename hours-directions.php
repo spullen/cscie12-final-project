47101e37eab398196b91a181b6145c4c5137a39f
Scott Pullen <s.pullen05@gmail.com>
2015-05-02 10:25:10 -0400

    Adding hours

--- hours-directions.php
+++ hours-directions.php
@@ -7,6 +7,28 @@
   <div class="grid-100">
     <h2>Hours</h2>
 
+    <p>
+      <strong>Monday</strong> - 8:00 a.m. until 5:00 p.m.<br>
+      <strong>Tuesday</strong> - 8:00 a.m. until 5:00 p.m.<br>
+      <strong>Wednesday</strong> - 8:00 a.m. until 5:00 p.m.<br>
+      <strong>Thursday</strong> - 8:00 a.m. until 5:00 p.m.<br>
+      <strong>Friday</strong> - 8:00 a.m. until 3:30 p.m.<br>
+      <strong>Saturday</strong> - Closed<br>
+      <strong>Sunday</strong> - Closed
+    </p>
+
+    <p>
+      Our office will be <strong>closed</strong> on the following days:<br>
+      Memorial Day<br>
+      Labor Day<br>
+      Halloween<br>
+      Thanksgiving<br>
+      Christmas Eve<br>
+      Christmas<br>
+      New Years Eve<br>
+      New Years Day
+    </p>
+    <hr>
   </div>
   <div class="grid-100">
     <h2>Directions</h2>
