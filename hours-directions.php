27eb9994be99dace31fa049c977b0e1085a58057
Scott Pullen <s.pullen05@gmail.com>
2015-05-02 10:59:32 -0400

    Adding directions

--- hours-directions.php
+++ hours-directions.php
@@ -6,7 +6,6 @@
 <div class="grid-container">
   <div class="grid-100">
     <h2>Hours</h2>
-
     <p>
       <strong>Monday</strong> - 8:00 a.m. until 5:00 p.m.<br>
       <strong>Tuesday</strong> - 8:00 a.m. until 5:00 p.m.<br>
@@ -16,10 +15,10 @@
       <strong>Saturday</strong> - Closed<br>
       <strong>Sunday</strong> - Closed
     </p>
-
     <p>
       Our office will be <strong>closed</strong> on the following days:<br>
       Memorial Day<br>
+      Independence Day</br>
       Labor Day<br>
       Halloween<br>
       Thanksgiving<br>
@@ -32,10 +31,27 @@
   </div>
   <div class="grid-100">
     <h2>Directions</h2>
-
+    <address>
+      123 Fake Street, Suite 100<br>
+      Cambridge, Massachusetts 02138
+    </address>
   </div>
   <div class="grid-100">
     <div id="map-container"></div>
+  </div>
+  <div class="grid-100">
+    <h3>Driving from South of Boston:</h3>
+    <p>
+      Take I-93 North to exit 26 (Cambridge/Storrow Drive) in the recently opened Big Dig tunnel. The exit ramp merges with traffic on Storrow Drive from the left. Move to one of the right lanes and continue on Storrow Drive approximately 4 miles to Cambridge/Allston. After a Doubletree Guest Quarters Hotel, take the right-hand exit to “Harvard Square - Cambridge.” Turn right at end of ramp onto John F. Kennedy Street. Harvard Square is approximately 1/2 mile straight ahead.
+    </p>
+    <h3>Driving from North of Boston:</h3>
+    <p>
+      Take I-93 South to exit 26A (Storrow Drive/North Station). Keep left and take the underpass to Storrow Drive. Continue on Storrow Drive approximately 4 miles to Cambridge/Allston. After a Doubletree Guest Quarters Hotel, take the right-hand exit to “Harvard Square - Cambridge.” Turn right at end of ramp onto John F. Kennedy Street. Harvard Square is approximately 1/2 mile straight ahead.
+    </p>
+    <h3>Driving from West of Boston:</h3>
+    <p>
+      Take I-90 East (Mass Turnpike) to exit 18 (Cambridge/Allston). Bear right after toll booth at end of exit ramp. Turn left after lights (before the bridge) onto Storrow Drive. Take Cambridge/Allston exit (1/2 mile ahead) and turn right at end of ramp onto John F. Kennedy Street. Harvard Square is approximately 1/2 mile straight ahead.
+    </p>
   </div>
 </div>
 <?php include 'includes/footer.php' ?>
