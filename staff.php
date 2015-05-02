b9d6c99fbfbed32a5beb656fbfa30bd947045800
Scott Pullen <s.pullen05@gmail.com>
2015-05-02 10:03:26 -0400

    Adding staff photos

--- staff.php
+++ staff.php
@@ -9,16 +9,28 @@
   </div>
 </div>
 <div class="grid-container staff-profile">
-  <div class="grid-25" style="border: 1px solid red;"><img src="images/staff/staff1.jpg" alt="" /></div>
-  <div class="grid-75" style="border: 1px solid blue;">SHOULD</div>
+  <div class="grid-25 image-container"><img src="images/staff/staff1.jpg" alt="" /></div>
+  <div class="grid-75">
+    <h3>Name</h3>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean suscipit enim tellus, sed fermentum nisi volutpat vel. Etiam sed congue nunc. Vivamus rhoncus risus non sagittis maximus. Nunc mollis ligula quam, quis pretium tellus porta vel. Quisque commodo ipsum vulputate urna euismod dapibus. Donec egestas erat non nibh elementum, et mollis lorem fringilla. Pellentesque volutpat varius leo, sit amet accumsan augue egestas sed. Proin sit amet dolor ac mauris eleifend lobortis. Praesent a consectetur purus. Aenean efficitur quis velit non placerat. Etiam tristique erat vitae ex dignissim commodo. Donec iaculis, dolor quis elementum porttitor, risus tortor varius leo, vel venenatis diam purus eu dolor. Quisque felis nulla, faucibus ut nisl id, posuere tempus nulla. Curabitur vehicula lobortis metus id interdum. Maecenas luctus leo nec sem fermentum euismod. Donec pulvinar leo quis dapibus pharetra.</p>
+    <p>Phasellus maximus iaculis sapien, vel sodales nisi vestibulum vitae. Integer a velit dictum, luctus tellus nec, eleifend enim. Quisque vitae purus vel velit sodales eleifend efficitur ac metus. Sed purus tellus, tristique ac feugiat sed, molestie interdum sem. Aliquam nec leo vitae elit semper ornare. Nam eu ex sapien. Etiam non eleifend nisi, quis eleifend lorem. Vivamus ultricies, urna sed posuere mollis, turpis tellus feugiat mauris, sed facilisis arcu risus sit amet magna. Suspendisse libero est, sagittis id elementum at, imperdiet vel lacus. Donec in neque sed eros condimentum consequat. Nulla laoreet dictum tempus. Fusce in ex mi. Suspendisse tincidunt id nisl eget pretium.</p>
+  </div>
 </div>  
 <div class="grid-container staff-profile">  
-  <div class="grid-25" style="border: 1px solid green;"><img src="images/staff/staff2.jpg" alt="" /></div>
-  <div class="grid-75" style="border: 1px solid orange;">HERE</div>
+  <div class="grid-25 image-container"><img src="images/staff/staff2.jpg" alt="" /></div>
+  <div class="grid-75">
+    <h3>Name</h3>
+    <p>In venenatis, elit in porta scelerisque, quam eros tempus ante, rutrum pellentesque dolor quam sed diam. Ut feugiat at tortor vitae consectetur. Nam hendrerit diam augue, at consequat ipsum dictum quis. Proin eleifend quam a est scelerisque, eu sodales tortor accumsan. Aenean at tristique mi, in suscipit lectus. Sed aliquam risus ullamcorper, fringilla enim quis, lobortis dui. Vestibulum in est tempus, scelerisque nisi ut, dignissim turpis. Donec mattis rutrum neque, ac fermentum ex bibendum at. Duis efficitur magna vel purus ornare, ut consequat lacus ullamcorper. Sed quis urna lectus. Nam non odio bibendum, finibus ligula a, dapibus augue. Mauris viverra risus sed faucibus venenatis. Nulla tristique enim diam, pellentesque lobortis sapien vestibulum vel. Cras gravida mauris mattis est finibus tincidunt. Curabitur imperdiet congue sollicitudin.</p>
+    <p>Donec porttitor odio vel velit suscipit congue. Nam leo massa, faucibus sed vulputate in, bibendum ut eros. Quisque molestie ligula risus, in dignissim orci condimentum consequat. Donec consectetur in quam a sagittis. In sit amet justo ligula. Ut consectetur sagittis eros vel placerat. Maecenas laoreet sit amet quam vitae eleifend. Nulla facilisi. Ut et elit bibendum, mollis mi sed, aliquet neque. In sem risus, aliquam non enim at, rutrum auctor nulla. Aenean congue tristique sapien et pretium. Morbi nec orci at ex posuere volutpat quis ut arcu.</p>
+  </div>
 </div>  
 <div class="grid-container staff-profile">  
-  <div class="grid-25" style="border: 1px solid purple;"><img src="images/staff/staff3.jpg" alt="" /></div>
-  <div class="grid-75" style="border: 1px solid brown;">Fill</div>
+  <div class="grid-25 image-container"><img src="images/staff/staff3.jpg" alt="" /></div>
+  <div class="grid-75">
+    <h3>Name</h3>
+    <p>Mauris nec sem quis velit tincidunt ornare. Vivamus nibh diam, dictum dapibus lacus et, laoreet ullamcorper metus. Proin congue, tortor id vehicula dapibus, felis dolor maximus orci, nec molestie est mauris quis velit. Aenean risus neque, ornare sed orci id, consectetur convallis ante. Nunc sagittis nibh nisi, vel molestie ligula auctor eget. Nunc molestie hendrerit interdum. Cras faucibus vestibulum libero ac consequat.</p>
+    <p>Nulla tristique, libero eu euismod mollis, sapien quam tincidunt arcu, nec tristique dolor nibh quis nunc. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed blandit consequat quam, in malesuada arcu interdum at. Phasellus porta lacus quis massa sollicitudin eleifend. Sed leo neque, pulvinar ut aliquet luctus, ullamcorper ac felis. Donec at ullamcorper purus. Proin blandit quam ac sapien vehicula aliquet. Pellentesque elementum auctor nisi, porttitor ornare tellus commodo nec. Morbi quis ultrices sem. Aliquam luctus sem a risus venenatis, in mollis magna rutrum. Nulla iaculis orci vitae nulla sollicitudin semper. Quisque et sollicitudin lorem.</p>
+  </div>
 </div>  
 <div class="grid-container">
   <div class="grid-100">
@@ -26,20 +38,32 @@
   </div>
 </div>
 <div class="grid-container staff-profile">
-  <div class="grid-25" style="border: 1px solid red;"><img src="images/staff/staff4.jpg" alt="" /></div>
-  <div class="grid-75" style="border: 1px solid blue;">SHOULD</div>
+  <div class="grid-25 image-container"><img src="images/staff/staff4.jpg" alt="" /></div>
+  <div class="grid-75">
+    <h3>Name</h3>
+    <p>In scelerisque lobortis odio, sit amet finibus odio ornare vel. Aliquam convallis, nisi non vehicula pellentesque, libero urna tempor lectus, eget vestibulum nunc mi sed sem. Nullam vel urna at felis dapibus euismod sed vel metus. Nulla id posuere dui. Cras condimentum, diam in tincidunt imperdiet, felis magna varius erat, ut finibus purus velit ut libero. Vestibulum mauris mi, ultricies id massa id, finibus gravida dui. Praesent non mattis nibh. Integer imperdiet erat eu mattis consectetur. Nam nec dictum eros. Integer non pellentesque enim.</p>
+  </div>
 </div>  
 <div class="grid-container staff-profile">  
-  <div class="grid-25" style="border: 1px solid green;"><img src="images/staff/staff5.jpg" alt="" /></div>
-  <div class="grid-75" style="border: 1px solid orange;">HERE</div>
+  <div class="grid-25 image-container"><img src="images/staff/staff5.jpg" alt="" /></div>
+  <div class="grid-75">
+    <h3>Name</h3>
+    <p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Suspendisse faucibus condimentum neque vel finibus. Duis bibendum elit non malesuada porttitor. Aliquam ullamcorper auctor leo, eu aliquet elit dignissim id. Ut dictum mi sed odio ultrices, dignissim ornare neque ornare. Mauris id luctus nulla. Morbi quis tellus felis. Fusce facilisis ornare hendrerit. Vivamus pulvinar convallis iaculis. Aenean finibus eros a ultrices pretium. Maecenas convallis vitae ipsum a sollicitudin. Aliquam non est a neque fermentum facilisis vel a ante. Curabitur ultricies, erat sit amet consequat maximus, eros tellus porta mauris, ac imperdiet dui diam a urna. Nullam aliquet nibh mi, non pharetra mauris tempus sit amet.</p>
+  </div>
 </div>  
 <div class="grid-container staff-profile">  
-  <div class="grid-25" style="border: 1px solid purple;"><img src="images/staff/staff6.jpg" alt="" /></div>
-  <div class="grid-75" style="border: 1px solid brown;">Fill</div>
+  <div class="grid-25 image-container"><img src="images/staff/staff6.jpg" alt="" /></div>
+  <div class="grid-75">
+    <h3>Name</h3>
+    <p>Aliquam sit amet feugiat diam, eget venenatis velit. Morbi ut massa et urna euismod aliquet eu in ante. Nullam ac tempor diam. Morbi feugiat magna quis nisl vulputate, a dapibus erat mattis. In consequat auctor tincidunt. Mauris euismod, quam vitae cursus pharetra, massa ligula ultricies tortor, accumsan placerat nulla arcu non nunc. Nunc bibendum massa in lobortis interdum. Mauris lacinia arcu ac lectus rutrum, blandit auctor leo consequat. Morbi faucibus semper viverra. Sed ornare risus odio, in tempor tellus faucibus in. Vivamus suscipit non neque eu congue. Quisque at dui ac enim lobortis luctus.</p>
+  </div>
 </div>
 <div class="grid-container staff-profile">  
-  <div class="grid-25" style="border: 1px solid purple;"><img src="images/staff/staff9.jpg" alt="" /></div>
-  <div class="grid-75" style="border: 1px solid brown;">Fill</div>
+  <div class="grid-25 image-container"><img src="images/staff/staff9.jpg" alt="" /></div>
+  <div class="grid-75">
+    <h3>Name</h3>
+    <p>Cras sagittis augue eu ex mattis faucibus. Phasellus eu elit ex. Curabitur sed mauris et enim sodales laoreet ut non urna. Fusce congue vestibulum turpis. Ut feugiat mi laoreet, eleifend est non, luctus tellus. Phasellus viverra felis eget aliquet tempus. Sed euismod augue in orci convallis, eget lobortis est gravida. Cras lacus ante, varius at ex vel, consectetur euismod dui. Morbi sit amet euismod ligula. Nunc dui ante, rutrum tincidunt massa eget, viverra faucibus ipsum.</p>
+  </div>
 </div>
 <div class="grid-container">
   <div class="grid-100">
@@ -47,11 +71,17 @@
   </div>
 </div>
 <div class="grid-container staff-profile">  
-  <div class="grid-25" style="border: 1px solid purple;"><img src="images/staff/staff7.jpg" alt="" /></div>
-  <div class="grid-75" style="border: 1px solid brown;">Fill</div>
+  <div class="grid-25 image-container"><img src="images/staff/staff7.jpg" alt="" /></div>
+  <div class="grid-75">
+    <h3>Name</h3>
+    <p>Aenean finibus convallis consequat. Sed sit amet tellus ex. Suspendisse quam libero, scelerisque quis bibendum in, iaculis eu quam. Curabitur eu nulla sodales, luctus nunc at, scelerisque eros. Morbi vel risus quis quam dignissim blandit eu id urna. Nulla auctor, nisi at volutpat ultrices, lorem tellus egestas lorem, ac rutrum turpis dui a eros. Quisque sit amet vehicula leo. Donec tristique dolor vel velit fringilla auctor ac et dui. Donec malesuada id odio in finibus. Proin consectetur efficitur ex auctor lobortis. Quisque posuere neque vitae lacus rhoncus luctus. Proin in aliquam est. Nunc malesuada tincidunt risus id auctor. In iaculis nec odio consectetur rutrum.</p>
+  </div>
 </div>
 <div class="grid-container staff-profile">  
-  <div class="grid-25" style="border: 1px solid green;"><img src="images/staff/staff8.jpg" alt="" /></div>
-  <div class="grid-75" style="border: 1px solid orange;">HERE</div>
+  <div class="grid-25 image-container"><img src="images/staff/staff8.jpg" alt="" /></div>
+  <div class="grid-75">
+    <h3>Name</h3>
+    <p>Cras in molestie orci, fermentum molestie tortor. Proin imperdiet leo ac imperdiet volutpat. Vestibulum rhoncus sem non tortor suscipit, eu tempor neque varius. Praesent sed euismod justo, sed auctor dolor. Sed pretium odio leo, eget molestie est scelerisque vitae. Aliquam erat volutpat. Etiam pulvinar mauris ac elementum mollis. Ut eleifend, tellus in sollicitudin laoreet, ante mauris consequat nibh, semper mollis sem leo sit amet leo. Sed viverra porta sapien, nec blandit odio condimentum ac. Nullam tempor facilisis nibh. Sed nibh dolor, pharetra nec tempor vitae, auctor non ligula. Pellentesque efficitur velit eros, at luctus massa posuere in.</p>
+  </div>
 </div>  
 <?php include 'includes/footer.php' ?>
