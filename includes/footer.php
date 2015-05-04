dffbb71559952b923fb80c6262dcf9d69d7818ad
Scott Pullen <s.pullen05@gmail.com>
2015-05-04 19:56:37 -0400

    Adding footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,14 @@
+  <div class="grid-100">
+    <hr class="spacer">
+    <footer>
+      <p>
+        123 Fake Street, Suite 100<br>
+        Cambridge, Massachusetts 02138<br>
+        617-888-9999
+      </p>
+    </footer>
+  </div>
+</div>
 <script type="text/javascript" src="https://code.jquery.com/jquery-1.11.2.min.js"></script>
 <script type="text/javascript" src="https://code.jquery.com/ui/1.11.4/jquery-ui.min.js"></script>
 <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBLjKBscagzgDoqAnUwhsnPfzt245RmW1E"></script>
