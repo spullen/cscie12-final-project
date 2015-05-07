7c4ca86779bd9c8d144f7f31a118818b4286019a
Scott Pullen <s.pullen05@gmail.com>
2015-05-06 22:03:26 -0400

    Fixing html validation error on contact us page

--- contact.php
+++ contact.php
@@ -10,13 +10,11 @@
   <h3>Fax:</h3>
   <p>617-999-8888</p>
   <h3>Office Address:</h3>
-  <p>
-    <address>
-      Cambridge Pediatric Dental<br>
-      123 Fake Street, Suite 100<br>
-      Cambridge, Massachusetts 02138
-    </address>
-  </p>
+  <address>
+    Cambridge Pediatric Dental<br>
+    123 Fake Street, Suite 100<br>
+    Cambridge, Massachusetts 02138
+  </address>
   <h3>Email:</h3>
   <p><a href="mailto:info@pediatricdentist.com">info@pediatricdentist.com</a></p>
   <p>You should receive a reply within 48 business hours. If you do not receive a reply, please call to talk with one of our receptionists</p>
