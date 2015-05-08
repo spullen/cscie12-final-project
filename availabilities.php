c4510d5e23cab3ac67287218c612d203cfd63777
Scott Pullen <s.pullen05@gmail.com>
2015-05-07 20:44:54 -0400

    5.3.3 php compat

--- availabilities.php
+++ availabilities.php
@@ -8,19 +8,18 @@
   $start = strtotime($_GET['appointmentDate'] . " 07:00");
   $end = strtotime($_GET['appointmentDate'] . " 17:00");
 
-  $times = [];
+  $times = array();
   for($i = $start; $i <= $end; $i += 1800) {
     array_push($times, date('m/d/Y H:i:s', $i));
   }
 
   $timeIndices = array_rand($times, rand(2, 7));
 
-  $availabilities = [];
+  $availabilities = array();
   foreach($timeIndices as $i) {
     array_push($availabilities, $times[$i]);
   }
 
   header('Content-Type: application/json');
-  http_response_code();
   echo json_encode($availabilities);
 ?>
